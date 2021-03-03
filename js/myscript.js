c79ad5dfc24626ec454aaea94569596b5a500f2e
Kseniya <ks.nowik@yandex.ru>
2021-03-03 14:20:50 +0300

    второе практическое занятие

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const numberOfFilms = prompt('Сколько фильмов вы посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,12 +10,29 @@ const personalMovieDB = {
     privat: false
 };
 
-const q1 = prompt('Один из последних просмотренных фильмов?', ''),
-    q2 = prompt('На сколько оцените его?', ''),
-    q3 = prompt('Один из последних просмотренных фильмов?', ''),
-    q4 = prompt('На сколько оцените его?', '');
+for (let i = 1; i <=2; i++) {
+    
+    const q1 = prompt('Один из последних просмотренных фильмов?', ''),
+          q2 = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[q1] = q2;
-personalMovieDB.movies[q3] = q4;
+
+    if(q1.length < 50 && q1 != null && q1 != '' && q2.length < 50 && q2 != null && q2 != '') {
+        personalMovieDB.movies[q1] = q2;
+        console.log('done');
+    } else {
+        i--;
+    }
+    
+}
+
+if(personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <=30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
